d492939a9e05df84d6883e271c5931290971d9fe
tuguldur <aa402115@gmail.com>
2020-04-07 11:20:38 +0800

    update backend boilerplate

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+const mongodb = process.env.MONGO;
+app.use(express.json());
+app.use("/", (req, res) => {
+  res.json({ status: true });
+});
+app.listen(PORT, () => {
+  mongoose
+    .connect(mongodb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+  mongoose.set("useCreateIndex", true);
+  console.log(`server running on the port: ${PORT}`);
+});
